3bdc8d8f5f6c2208fd99626967f6379257b0997c
asen23 <andersenlim23@gmail.com>
2021-08-31 16:49:33 +0700

    Feat: added homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react"
 
 function App() {
-    return <h1>Hello World!</h1>
+    return (
+        <div className="flex flex-column items-center justify-center vh-100 bg-lightest-blue">
+            <h1 className="f1">Fun Trivia</h1>
+            <button className="bg-blue b--light-blue br3 f2 pa2 ma2 grow">
+                Play
+            </button>
+            <button className="bg-blue b--light-blue br3 f2 pa2 ma2 grow">
+                Score
+            </button>
+        </div>
+    )
 }
 
 export default App
