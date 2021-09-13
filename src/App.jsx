3a378329bfb38fe184d836ac8ad4c7c0beed3869
Andersen <andersenlim23@gmail.com>
2021-09-13 10:53:08 +0700

    ref: removed redundant react import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Switch, Route, useHistory } from "react-router-dom"
 import Button from "./Components/Button"
 import Trivia from "./Components/Trivia"
